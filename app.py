e3f43f0e40612761d9a3cb9ebe454f39773d2cf8
Aniruth D Coder <122196847+AniruthDCoder@users.noreply.github.com>
2023-09-28 15:52:00 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 from flask import Flask
-app = Flask(name)
+app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'https://github.com/AniruthDCoder'
+    return 'https://www.youtube.com/@GreyMattersYT'
 
 
-if name == "main":
+if __name__ == "__main__":
     app.run()
